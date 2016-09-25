01751e173fedd6fcd90a1b1ecfbfb6b54df79a9f
Kash Raman <raman.kasthuri@gmail.com>
2016-09-25 12:54:12 -0400

    adding router

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,28 @@
-  var app = angular.module('todoApp',[]);
-
-  app.controller('twoWayBindingCtrl', function twoWayBindingCtrl($scope){
-//    $scope.todos = [{name="Tomatoes", store= "american"},  {name="Curry leaves", store= "indian"}];
-    $scope.fullname = $scope.first + " " +$scope.last;
+  var app = angular.module('todoApp', ["ngRoute"]);
+  app.run(function($rootScope){
+    $rootScope.test="Hello root";
+  });
+  app.config(function($routeProvider){
+    $routeProvider.when("/", {
+      template : "<H1> welcome</H1>"
+    })
+    .when("/binder", {
+      templateUrl : "1bind.html"
+    }).when("/one", {
+      templateUrl : "test.html"
+    });
+  });
+  app.controller('twoWayBindingCtrl', function($scope) {
+    $scope.test="Hello control";
+      $scope.fullname = function() {
+          return $scope.first + " " + $scope.last;
+      };
+  });
+  app.controller('loopme', function($scope) {
+      $scope.names = ['Jani', 'Hege', 'Kai'];
+  });
+  app.directive("loopDirective", function() {
+      return {
+          template: "Utilizing custom directive and ng-repeat"
+      };
   });
